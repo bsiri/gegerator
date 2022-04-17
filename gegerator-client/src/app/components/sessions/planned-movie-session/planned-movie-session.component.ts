ca3aeb76aa44c6e32e3a53b3122187aa097bc354
Benoit SIRI <bsiri@NXQLX048.nxdomain.lan>
2022-04-17 23:25:20 +0200

    can now edit and delete sessions

--- gegerator-client/src/app/components/sessions/planned-movie-session/planned-movie-session.component.ts
+++ gegerator-client/src/app/components/sessions/planned-movie-session/planned-movie-session.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
 import { MovieSession, PlannedMovieSession } from 'src/app/models/session.model';
+import { SessionActions } from 'src/app/ngrx/actions/session.actions';
+import { ConfirmOutput, GenericPurposeDialog } from '../../genericpurposedialog/genericpurposedialog.component';
 import { SESSION_DAY_BOUNDARIES } from '../session-day-boundaries.model';
+import { SessionDialog } from '../sessiondialog/sessiondialog.component';
 
 @Component({
   selector: 'app-planned-movie-session',
@@ -14,8 +19,8 @@ export class PlannedMovieSessionComponent implements OnInit {
   heightInPixel: string = '0px'
   topPosInPixel: string = '0px'
 
-  constructor() {
-   }
+  constructor(private store: Store, private dialog: MatDialog) {
+  }
 
   ngOnInit(): void {
     // must wait for 'session' to be set before computing dimensions
@@ -24,11 +29,46 @@ export class PlannedMovieSessionComponent implements OnInit {
   }
 
   updateSession(){
-    //placeholder
+    const _clone = { ...this.session} as PlannedMovieSession
+    const dialogRef = this.dialog.open(SessionDialog, {
+      autoFocus: 'first-tabbable',
+      data: _clone
+    })
+
+    dialogRef.afterClosed().subscribe(updatedSessionData =>{
+      if (! updatedSessionData){
+        return 
+      }
+      const session = _toMovieSession(updatedSessionData)
+      this.store.dispatch(SessionActions.update_session({session}))
+    })
   }
 
   confirmThenDelete(){
-    //placeholder
+    const dialogRef = this.dialog.open(GenericPurposeDialog, {
+      data: {
+        html: `Vraiment supprimer cette session ?`,
+        type: "confirm"
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(response =>{
+      if (response == ConfirmOutput.CONFIRM){
+        const session = _toMovieSession(this.session)
+        this.store.dispatch(SessionActions.delete_session({session}))  
+      }
+    })
   }
 
+}
+
+function _toMovieSession(pms: PlannedMovieSession): MovieSession{
+  const session: MovieSession = {
+    id: pms.id, 
+    movieId: pms.movie.id,
+    theater: pms.theater,
+    startTime: pms.startTime,
+    day: pms.day
+  }
+  return session
 }
