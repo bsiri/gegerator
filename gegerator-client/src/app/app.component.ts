6801d8bfc50e2ec04018fed5623b5d06d8cb55a5
Benoit SIRI <bsiri@NXQLX048.nxdomain.lan>
2022-05-15 18:05:10 +0200

    can now export the roadmap as a downloadable plain text file

--- gegerator-client/src/app/app.component.ts
+++ gegerator-client/src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import { UploadDialog } from './components/appstate/uploaddialog/uploaddialog.component';
+import { OtherActivity } from './models/activity.model';
+import { PlannableEvent } from './models/plannable.model';
+import { FestivalRoadmap } from './models/roadmap.model';
+import { PlannedMovieSession } from './models/session.model';
 import { AppStateActions } from './ngrx/actions/appstate.actions';
+import { selectUserRoadmap } from './ngrx/selectors/roadmap.selectors';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +15,8 @@ import { AppStateActions } from './ngrx/actions/appstate.actions';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  title = "Gegerator"
+
+  roadmap!: FestivalRoadmap
 
   constructor(private store: Store, 
     private dialog: MatDialog
@@ -18,6 +24,9 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.store.dispatch(AppStateActions.reload_appstate())
+    this.store.select(selectUserRoadmap).subscribe(rm => this.roadmap = rm)
+    // Note : no need to take care of unsubscribing here since destroying the app
+    // would essentially entail the completion of all the observables
   }
 
   uploadAppState(): void{
@@ -31,6 +40,33 @@ export class AppComponent implements OnInit{
         this.store.dispatch(AppStateActions.upload_appstate({file}));
       }
     });
+  }
+
+  exportRoadmap(){
+    // Build the textual representation of the Roadmap
+    const planningMap = this.roadmap.dailyPlanning()
+    let planningStr = "=== Roadmap Gérardmer ===\n\n"
+
+    for (let entry of planningMap.entries()) {
+      const [day, events] = entry
+      planningStr += `${day.name} :\n` + events.map(this.formatEventStr)
+      planningStr += '\n'
+    }
+
+    // Simulate a download, thanks SO question 2897619
+    // with a bit of html dirty tricks
+    const fakelink = document.createElement('a')
+    fakelink.download = 'roadmap.txt'
+    fakelink.href = "data:application/octet-stream," + encodeURIComponent(planningStr)
+    fakelink.click()
+
+  }
+
+  // quick and dirty way to remove the day from the 'toString()' output,
+  // but that's fragile and would never pass CR if there were one. But hey, 
+  // that's my pet project so who cares :-)
+  formatEventStr(event: PlannableEvent): string{
+    return event.toString().replace(/^(.*?), /, '    ') + '\n'
   }
 
 }
