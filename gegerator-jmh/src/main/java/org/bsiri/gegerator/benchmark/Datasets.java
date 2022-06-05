3a20a346f24af0398fa21c3ef533178e4f6e5259
Benoit SIRI <bsiri@NXQLX048.nxdomain.lan>
2022-06-05 16:48:43 +0200

    skeletton microbenchmark

---
+++ gegerator-jmh/src/main/java/org/bsiri/gegerator/benchmark/Datasets.java
@@ -0,0 +1,54 @@
+package org.bsiri.gegerator.benchmark;
+
+import org.bsiri.gegerator.services.planner.PlannerEvent;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import static org.bsiri.gegerator.services.planner.PlannerEventHelper.*;
+
+public class Datasets {
+    static private Long NO_MOVIE = null;
+    static private Long MOVIE_1 = 1L;
+    static private Long MOVIE_2 = 2L;
+    static private Long MOVIE_3 = 3L;
+    static private Long MOVIE_4 = 4L;
+
+    static private long OUTSTANDING_SCORE = 100000;
+    static private long SUPER_SCORE = 1000;
+    static private long MEDIUM_SCORE = 500;
+    static private long AVERAGE_SCORE = 0;
+    static private long AWFUL_SCORE = -1000;
+
+
+    public static List<PlannerEvent> shuffledSmallDataset(){
+        List<PlannerEvent> events = Arrays.asList(
+            // THURSDAY
+            event("movie 1 awfull", AWFUL_SCORE, MOVIE_1, "PARADISO | THURSDAY | 08:00 | 09:00"),
+            event("movie 2 super", SUPER_SCORE, MOVIE_2, "ESPACE_LAC | THURSDAY | 10:00 | 11:00"),
+            event("movie 3 medium", MEDIUM_SCORE, MOVIE_3, "CASINO | THURSDAY | 12:00 | 13:00"),
+            event("movie 4 average", AVERAGE_SCORE, MOVIE_4, "MCL | THURSDAY | 14:00 | 15:00"),
+
+            // FRIDAY
+            event("movie 2 awfull", AWFUL_SCORE, MOVIE_2, "PARADISO | FRIDAY | 08:00 | 09:00"),
+            event("movie 3 super", SUPER_SCORE, MOVIE_3, "ESPACE_LAC | FRIDAY | 10:00 | 11:00"),
+            event("movie 4 medium", MEDIUM_SCORE, MOVIE_4, "CASINO | FRIDAY | 12:00 | 13:00"),
+            event("movie 1 average", AVERAGE_SCORE, MOVIE_1, "MCL | FRIDAY | 14:00 | 15:00"),
+
+            // SATURDAY + RESTAURANT
+            event("movie 3 awfull", AWFUL_SCORE, MOVIE_3, "PARADISO | SATURDAY | 08:00 | 09:00"),
+            event("movie 4 super", SUPER_SCORE, MOVIE_4, "ESPACE_LAC | SATURDAY | 10:00 | 11:00"),
+            event("restaurant", OUTSTANDING_SCORE, NO_MOVIE, "| SATURDAY | 10:30 | 11:30"),
+            event("movie 1 medium", MEDIUM_SCORE, MOVIE_1, "CASINO | SATURDAY | 12:00 | 13:00"),
+            event("movie 2 average", AVERAGE_SCORE, MOVIE_2, "MCL | SATURDAY | 14:00 | 15:00"),
+
+            // SUNDAY
+            event("movie 4 awfull", AWFUL_SCORE, MOVIE_4, "PARADISO | SUNDAY | 08:00 | 09:00"),
+            event("movie 1 super", SUPER_SCORE, MOVIE_1, "ESPACE_LAC | SUNDAY | 10:00 | 11:00"),
+            event("movie 2 medium", MEDIUM_SCORE, MOVIE_2, "CASINO | SUNDAY | 12:00 | 13:00"),
+            event("movie 3 average", AVERAGE_SCORE, MOVIE_3, "MCL | SUNDAY | 14:00 | 15:00")
+        );
+        Collections.shuffle(events);
+        return events;
+    }
+}
---
+++ gegerator-jmh/src/main/java/org/bsiri/gegerator/benchmark/PlannerBenchmark.java
@@ -0,0 +1,63 @@
+package org.bsiri.gegerator.benchmark;
+
+
+import ch.qos.logback.core.joran.spi.EventPlayer;
+import org.bsiri.gegerator.services.planner.GraphBasedPlanner;
+import org.bsiri.gegerator.services.planner.PlannerEvent;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 2, time = 5, timeUnit = TimeUnit.MICROSECONDS)
+@Measurement(iterations = 5, time = 25, timeUnit = TimeUnit.MICROSECONDS)
+public class PlannerBenchmark {
+
+    @State(Scope.Benchmark)
+    public static class DatasetState{
+        List<PlannerEvent>  events = new ArrayList<PlannerEvent>();
+
+        @Setup(Level.Iteration)
+        public void newEvents(){
+            this.events = Datasets.shuffledSmallDataset();
+        }
+
+    }
+
+
+    @Benchmark
+    public List<PlannerEvent> benchGraphBasedPlanner(DatasetState dataset){
+        return new GraphBasedPlanner(dataset.events).findBestRoadmap();
+    }
+
+    /*
+     * ============================== HOW TO RUN THIS TEST: ====================================
+     *
+     * Note JMH honors the default annotation settings. You can always override
+     * the defaults via the command line or API.
+     *
+     * You can run this test:
+     *
+     * a) Via the command line:
+     *    $ mvn clean install
+     *    $ java -jar target/microBenchmarks.jar PlannerBenchmark
+     *
+     * b) Via the Java API:
+     *    (see the JMH homepage for possible caveats when running from IDE:
+     *      http://openjdk.java.net/projects/code-tools/jmh/)
+     */
+    public static void main(String[] args) throws RuntimeException, RunnerException {
+        Options opts = new OptionsBuilder()
+                .include(PlannerBenchmark.class.getSimpleName())
+                .build();
+        new Runner(opts).run();
+    }
+
+
+}
