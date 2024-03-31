8339b22b2be4926d4c3c3956ab1bc213e77328c4
PQ5778 <benoit.siri@external.engie.com>
2024-03-31 01:17:43 +0100

    working on packaging and serving the webapp

--- Readme.md
+++ Readme.md
@@ -1,23 +1,36 @@
 Readme
 ---------
 
+Credits to https://github.com/FraktonDevelopers/spring-boot-angular-maven-build/blob/master/README.md for 
+explaining how to package the angular app in the server jar !
+
+TODO: also read https://www.baeldung.com/spring-webflux-static-content
+
 ### Any release yet ?
 
 No, and probably the tip of master branch is not stable. However a functional version 
-can be run if you revert to version `git checkout 1.0.0.PROTOTYPE`. See below for 
-instructions about how to run it.
+can be run if you revert to version `git checkout 1.0.0.PROTOTYPE`. See below 
+in the `Development` section for instructions about how to run it.
 
 
 ### Which java version ?
 
 Note sure, it's not configured in Maven :-O I recommend java 11 but it should run fine with java 8.
 
+### Which Node and NPM version ?
+
+Node server (for development): v16.13.0
+npm version: 8.1.0
+
+## Development
+
+These instruction apply for development, where a fully standalone distro is not required and 
+the various parts can/should be run separately.
 
 ### How do I build ?
 
 `mvn clean install` will build the backend server.
-`npm build` will build the webapp.
-
+`npm build` will build the webapp. 
 
 ### How do I run it ?
 
@@ -44,3 +57,7 @@ From within gegerator.client, run : `npm run startproxy`
 The file samples/gege_2020.json is a useful sample from the 2020 edition, with all movies and preferences set
 so you don't start from scratch everytime. You can upload it using the button with the Up/Down arrows icons. 
 
+
+## Create a distribution
+
+TODO
--- gegerator.server/src/main/java/org/bsiri/gegerator/config/GegeratorConfig.java
+++ gegerator.server/src/main/java/org/bsiri/gegerator/config/GegeratorConfig.java
@@ -1,12 +1,43 @@
 package org.bsiri.gegerator.config;
 
+import jdk.internal.loader.Resource;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.resource.PathResourceResolver;
+
+import java.io.IOException;
 
 @Configuration
 @EnableConfigurationProperties(Scoring.class)
 @EnableAspectJAutoProxy
 public class GegeratorConfig {
 
+    /*
+
+    TODO: http://[...]/gegerator/index.html serves the page just fine... but
+    the web assets are not taking care of the contextPath (the /gegerator)
+    So maybe the solution is to configure this thing.
+
+    Alternately see if we can serve the angular app with the base-href configured
+    as the contextPath
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                                : new ClassPathResource("/static/index.html");
+                    }
+                });
+    }
+
+     */
 }
