c015032af2b17f16e43c7d8b33f0775cc5cd0525
bsiri <benoit.siri@gmail.com>
2026-01-08 22:25:12 +0100

    got the test running.. all failed, but running

--- gegerator-client/vitest.config.ts
+++ gegerator-client/vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-
-   }
+    reporters: ['junit', 'json'],
+    outputFile: {
+      junit: './test-results/junit-report.xml',
+      json: './test-results/json-report.json'
+    }
+  }
 })
