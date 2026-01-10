157e8999778bbff0a291acae577b7a41a398304b
bsiri <benoit.siri@gmail.com>
2026-01-10 11:25:16 +0100

    test setup frontend is ready

---
+++ gegerator-client/src/test-setup.ts
@@ -0,0 +1,5 @@
+// Vitest setup to ensure Angular JIT compiler is available when needed
+import '@angular/compiler';
+import '@angular/platform-browser-dynamic';
+
+// Additional global test setup can be added here if required later.
