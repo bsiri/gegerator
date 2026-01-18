00b943082a4f15fa5b26b25463826180fce05d97
bsiri <benoit.siri@gmail.com>
2026-01-18 17:00:18 +0100

    removed the old tests, I was never going to use them anyway

--- gegerator-client/test-waiting-migration/order-by-lowercase.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OrderByLowercasePipe } from './order-by-lowercase.pipe';
-
-describe('OrderByLowercasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new OrderByLowercasePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
